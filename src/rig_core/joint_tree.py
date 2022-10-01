c9bd356eb921d5e571a86119f604cbcd872790f8
苍之幻灵 <www.cpcgskill.com>
2022-10-02 04:50:30 +0800

    feat(rig_core.joint_tree): 优化了蒙皮关节名称推导功能

--- src/rig_core/joint_tree.py
+++ src/rig_core/joint_tree.py
@@ -16,20 +16,29 @@ import cpmel.cmds as cc
 
 
 class Joint(object):
-    def __init__(self, obj=None, point=None, create_only=False, name=None):
+    def __init__(
+            self,
+            obj=None,
+            point=None,
+            create_only=False,
+            name=None,
+    ):
         if obj is not None:
             obj = cc.new_object(obj)
         self.obj = obj
         self.point = point
         self.name = name
-        # 如果这个属性为真将会仅创建，而不参与蒙皮
+
+        # 如果create_only属性为真将会仅创建，而不参与蒙皮
         self.create_only = create_only
 
+        self.parent = None
         self.childs = list()
 
     def add_childs(self, *jins):
         for jin in jins:
             self.childs.append(jin)
+            jin.parent = self
         return self
 
     def add_child_from_object(self, obj):
@@ -62,25 +71,46 @@ class Joint(object):
         root.create_only = are_end_joints_only_created
         return self
 
-    def add_joint_chain_from_object_list(self, objs,
-                                         are_end_joints_only_created=False,
-                                         extra_end_joint_of_point=None):
+    def add_joint_chain_from_object_list(self, objs, extra_end_joint_of_point=None):
         jins = [Joint(obj=i) for i in objs]
+        root = self
+        for jin in jins:
+            root.add_childs(jin)
+            root = jin
         if extra_end_joint_of_point is not None:
-            jins.append(Joint(point=extra_end_joint_of_point, create_only=True))
-        self.add_joint_chain(jins, are_end_joints_only_created)
+            root.add_childs(Joint(point=extra_end_joint_of_point, create_only=True))
         return jins
 
 
-def _create_real_joints_from_root(root, parent, control_table):
-    name = 'joint'
-    if root.create_only:
-        if parent is not None:
-            name = parent.name() + '_end'
+def get_joint_output_name(root):
+    if root.name is not None:
+        return root.name
+    name = None
     if isinstance(root.obj, cc.DagNode):
         name = root.obj.node_name() + '_skin_joint'
-    if root.name is not None:
-        name = root.name
+    if name is None and root.parent is not None:
+        if len(root.childs) < 1:
+            name = 'end_joint_of_' + get_joint_output_name(root.parent)
+        else:
+            name = 'child_joint_of_' + get_joint_output_name(root.parent)
+    if name is None:
+        if len(root.childs) < 1:
+            name = 'end_joint'
+        else:
+            name = 'joint'
+    return name
+
+
+def _create_real_joints_from_root(root, parent, control_table):
+    # name = 'joint'
+    # if root.create_only:
+    #     if parent is not None:
+    #         name = parent.name() + '_end'
+    # if isinstance(root.obj, cc.DagNode):
+    #     name = root.obj.node_name() + '_skin_joint'
+    # if root.name is not None:
+    #     name = root.name
+    name = get_joint_output_name(root)
     if parent is None:
         n = cc.createNode('joint', n=name)
     else:
